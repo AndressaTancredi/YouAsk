93570fc9d3f1ef37e51b7d3888ff3c6094a2d13d
Andressa Tancredi <andressa.tancredi@hotmail.com>
2019-06-24 14:28:44 -0300

    Commit Inicial

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+const servidor = express()
+const PORT = 3000
+
+servidor.use(cors())
+servidor.use(bodyParser.json())
+
+servidor.get('/', async (request, response) => {
+  response.send('Olá, mundo!')
+})
+
+
+servidor.listen(PORT)
+console.info(` Servidor rodando na porta ${PORT}`)
+
+
+
+
+
+
